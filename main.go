da2b3bacc5c007548256899c7d6d45653799f508
user <kravz2702@gmail.com>
2025-06-18 23:58:04 +0300

    simple routing

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	//"errors"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type book struct {
@@ -22,9 +24,87 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(200, books)
 }
 
+func bookByID(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	c.IndentedJSON(200, book)
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing id Query Parameter"})
+		return
+	}
+
+	book, err := getBookByID(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(200, book)
+}
+
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing id Query Parameter"})
+		return
+	}
+
+	book, err := getBookByID(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(200, book)
+}
+
+func getBookByID(id string) (*book, error) {
+	for i, book := range books {
+		if book.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	fmt.Printf("Book %+v\n", newBook)
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+	router.GET("/books/:id", bookByID)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
 	router.Run(":8080")
 
 }
